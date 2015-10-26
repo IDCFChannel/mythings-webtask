fe7f3a074159e435740070a3053b7c75c57e94c8
Masato Shimizu <ma6ato@gmail.com>
2015-10-26 12:03:26 +0900

    first commit

---
+++ slack.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var request = require('request');
+
+module.exports = function(ctx, cb) {
+    request.post({
+        url: 'http://' + ctx.data.host + '/data/' + ctx.data.uuid,
+        headers: {
+            'meshblu_auth_uuid': ctx.data.uuid,
+            'meshblu_auth_token': ctx.data.token
+        },
+        form: {
+            'time': new Date().toISOString()
+        }
+    });
+};
